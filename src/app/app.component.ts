94e305945f01052c54594cd9b6f2cbeffe079733
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-08-01 11:08:31 +0200

    navbar introduced

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AmplifyAuthenticatorModule],
+  imports: [NavbarComponent, RouterOutlet, AmplifyAuthenticatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
