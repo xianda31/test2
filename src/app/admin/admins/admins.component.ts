6dd6e63572b9982d476dcab9166c63c14917f48d
xianda31 <99276316+xianda31@users.noreply.github.com>
2024-08-03 15:24:34 +0200

    CR(U)DL implemented on administrators

--- src/app/admin/admins/admins.component.ts
+++ src/app/admin/admins/admins.component.ts
@@ -1,33 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { generateClient } from 'aws-amplify/data';
-import type { Schema } from '../../../../amplify/data/resource';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { generateClient, SelectionSet } from 'aws-amplify/data';
+import type { Schema } from '../../../../amplify/data/resource';
+// Remove the import statement for 'Nullable'
 
+// const selectionSet = ['id', 'email', 'username'] as const;
+// type Administrator = SelectionSet<Schema['Administrator']['type'], typeof selectionSet>;
+
+type Administrator = {
+  id: string | null;
+  email: string | null;
+  username: string | null;
+}
 @Component({
   selector: 'app-admins',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './admins.component.html',
   styleUrl: './admins.component.scss'
 })
 export class AdminsComponent implements OnInit {
 
+  administrators: Administrator[] = [];
+
   adminForm: FormGroup = new FormGroup({
-    email: new FormControl('john.do@bcsto.fr', Validators.required),
-    username: new FormControl('john', Validators.required)
+    email: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required)
   });
 
   async ngOnInit(): Promise<void> {
-    console.log("admins ==> ");
+
     const client = generateClient<Schema>();
-    const result = await client.models.Administrator.list();
-    console.log("admins : ", result);
+    const { data: administrators, errors } = await client.models.Administrator.list();
+    this.administrators = administrators;
   }
 
   async createAdministrator() {
-    console.log("creating new Administrator ==> ", this.adminForm.value);
     const client = generateClient<Schema>();
-    await client.models.Administrator.create(this.adminForm.value);
+    const { data: newAdministrator, errors } = await client.models.Administrator.create(this.adminForm.value);
+
+    if (errors) {
+      console.error(errors);
+      return;
+    } else {
+      if (newAdministrator !== null) {
+        this.administrators.push({
+          id: newAdministrator.id!,
+          email: newAdministrator.email!,
+          username: newAdministrator.username!
+        });
+      }
+    }
+  }
+
+  async deleteAdministrator(administrator: any) {
+    const client = generateClient<Schema>();
+    const { data: deletedAdministor, errors } = await client.models.Administrator.delete(administrator);
+    this.administrators = this.administrators.filter((a) => a !== administrator);
   }
 }
 
